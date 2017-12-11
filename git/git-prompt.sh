24bc9b4c0cfa44939c9371e19f6061efc2dfefcf
JasonBuss <jason.buss@customerfx.com>
2017-12-11 11:44:02 -0600

    Fixed __git_ps1 prompt in mintty

--- git/git-prompt.sh
+++ git/git-prompt.sh
@@ -7,20 +7,19 @@
 # PS1="$PS1"'\[\033[36m\]'  	# change color to cyan
 # PS1="$PS1"'\[\033[37m\]'    	# change to white
 
-
-
-
 	PS1='\[\033]0;$TITLEPREFIX:$PWD\007\]' # set window title
-	PS1="$PS1"'\[\033[32m\]'       # change to green
-	PS1="$PS1"'\u'             # user@host<space>
-	PS1="$PS1"'\[\033[37m\]'    	# change to white
-	PS1="$PS1"' at '             # user@host<space>
-	PS1="$PS1"'\[\033[35m\]'    	# change to purple
-	PS1="$PS1"'\h'             # user@host<space>
-	PS1="$PS1"'\[\033[37m\]'    	# change to white
-	PS1="$PS1"' in '
-	PS1="$PS1"'\[\033[33m\]'       # change to brownish yellow
-	PS1="$PS1"'\W'                 # current working directory
+	PS1="$PS1"'\[\e[01;30m\]\t' # TimeStamp
+	PS1="$PS1"'`if [ $? = 0 ]; then echo "\[\e[32m\] ✔ "; else echo "\[\e[31m\] ✘ "; fi`' #Add check for valid or failed command	
+	PS1="$PS1"'\[\033[35m\]'	# change color
+	PS1="$PS1"'\u'              # current user
+	PS1="$PS1"'\[\033[37m\]'	# change color
+	PS1="$PS1"'@'				# '@'
+	PS1="$PS1"'\[\033[34m\]'    # change color
+	PS1="$PS1"'\h'             	# current host
+	PS1="$PS1"'\[\033[37m\]'    # change color
+	PS1="$PS1"' in '			# ' in '
+	PS1="$PS1"'\[\033[36m\]'    # change color
+	PS1="$PS1"'\W'              # current working directory
 
 	if test -z "$WINELOADERNOEXEC"
 	then
@@ -32,16 +31,13 @@
 		then
 			. "$COMPLETION_PATH/git-completion.bash"
 			. "$COMPLETION_PATH/git-prompt.sh"
-#			PS1="$PS1"'\[\033[36m\]'  # change color to cyan
-#			PS1="$PS1"'`__git_ps1`'   # bash function
-			# export PS1='\[\e[01;30m\]\t`if [ $? = 0 ]; then echo "\[\e[32m\] ✔ "; else echo "\[\e[31m\] ✘ "; fi`\[\e[00;37m\]\u\[\e[01;37m\]:`[[ $(git status 2> /dev/null) =~ Changes\ to\ be\ committed: ]] && echo "\[\e[33m\]" || echo "\[\e[31m\]"``[[ ! $(git status 2> /dev/null) =~ nothing\ to\ commit,\ working\ .+\ clean ]] || echo "\[\e[32m\]"`$(__git_ps1 "(%s)\[\e[00m\]")\[\e[01;34m\]\w\[\e[00m\]\$ '
+			 
+			PS1="$PS1"'`[[ $(git status 2> /dev/null) =~ Changes\ to\ be\ committed: ]] && echo "\[\e[33m\]" || echo "\[\e[31m\]"``[[ ! $(git status 2> /dev/null) =~ nothing\ to\ commit,\ working\ .+\ clean ]] || echo "\[\e[32m\]"`'
+			PS1="$PS1"'`__git_ps1`'   # bash function
+			
 		fi
 	fi
-
-#	PS1="$PS1"'\[\033[0m\]'        # change color
-	PS1="$PS1""\n"                 # new line
+	
+	PS1="$PS1"'\[\033[0m\]'        # change color
+	PS1="$PS1"'\n'                 # new line
 	PS1="$PS1""$ "                 # prompt: always $
-
-	PS1='\[\e[01;30m\]\t`if [ $? = 0 ]; then echo "\[\e[32m\] ✔ "; else echo "\[\e[31m\] ✘ "; fi`\[\e[00;32m\]\u\[\e[01;37m\] at \[\e[00;35m\]\h\[\e[01;37m\] `[[ $(git status 2> /dev/null) =~ Changes\ to\ be\ committed: ]] && echo "\[\e[33m\]" || echo "\[\e[31m\]"``[[ ! $(git status 2> /dev/null) =~ nothing\ to\ commit,\ working\ .+\ clean ]] || echo "\[\e[32m\]"`$(__git_ps1 "(%s) \[\e[00m\]")\[\e[01;34m\]\W\[\e[00m\] \$ '
-
-
